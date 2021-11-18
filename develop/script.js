4d54f7d58cb25079b2ec28f2d55faa24be5291c1
JideOgun <JideOgun@github.com>
2021-11-18 12:12:40 -0600

    figured out how to get my text to show up in the password text area

--- develop/script.js
+++ develop/script.js
@@ -1,13 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// generate random password
+let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+var generatePassword = function(){
+  return 'ddd';
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password);
+   //add password to textbox/display area
+document.getElementById("password").value = generatePassword();
 }
 
 // Add event listener to generate button
