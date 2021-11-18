58ee604713255a9c3cdbf0a6d80fe6a1026fd7b1
JideOgun <JideOgun@github.com>
2021-11-18 17:10:49 -0600

    figured out how to display random password based on user input from prompt

--- develop/script.js
+++ develop/script.js
@@ -5,18 +5,24 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword (length) { 
+  
   var result = '';
   var Char = 'ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+';
   var charLength = Char.length;
   for ( var i = 0; i < length; i++ ) {
-    result += Char.charAt(Math.floor(Math.random() * charLength));
+    result += Char.charAt(Math.floor(Math.random() * charLength));   
   }
   return result;
 }
 
  
 // Write password to the #password input
-function writePassword() {
+function writePassword() 
+{
+  //Length prompt 1
+  var lengthprompt = window.prompt('How long would you like your password to be?');
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -24,7 +30,7 @@ function writePassword() {
   
 
    //add password to textbox/display area
-document.getElementById("password").value = generatePassword(10);
+document.getElementById("password").value = generatePassword(lengthprompt);
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
